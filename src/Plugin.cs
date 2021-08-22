825d610a141427fb91f98df04c9e12d5383eb9a9
Chris Yeninas <844685+PhantomGamers@users.noreply.github.com>
2021-08-22 01:00:40 -0400

    initial patch implementation
    Screen.resolutions doesn't seem to actually include all resolutions so this doesn't currently work

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -1,6 +1,15 @@
 ﻿using BepInEx;
 
+using Code.Core.Progress;
+
 using HarmonyLib;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+
+using UnityEngine;
 
 namespace BepInExPlugin
 {
@@ -19,15 +28,101 @@ namespace BepInExPlugin
     [BepInPlugin(PluginInfo.PLUGIN_ID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
     public class Plugin : BaseUnityPlugin
     {
+        public static BepInEx.Logging.ManualLogSource log;
         private void Awake()
         {
+            log = Logger;
+            SetupResolutions();
             Harmony.CreateAndPatchAll(typeof(Patches));
+        }
+
+        public static void SetupResolutions()
+        {
+            log.LogInfo("Unlocking all resolutions...");
+            var resolutions = Code.Platform.PlatformManager.ResolutionSettings;
+            resolutions.Clear();
+            foreach (var res in Screen.resolutions)
+            {
+                var resolution = new Code.Platform.PlatformManager.ResolutionSetting()
+                {
+                    DisplayName = $"{res.width}x{res.height}",
+                    Width = res.width,
+                    Height = res.height
+                };
+                // Resolutions will be duplicated with different refresh rates, only add the first
+                if (resolutions.Where((currRes) => currRes.DisplayName == resolution.DisplayName).ToArray().Length == 0)
+                {
+                    log.LogDebug($"Adding resolution {resolution.DisplayName}");
+                    resolutions.Add(resolution);
+                }
+            }
+        }
+
+        public static void ClampResIdx(ref int aResIdx)
+        {
+            // Makes sure resolution index is within the bounds of the resolution options
+            var resSettings = Code.Platform.PlatformManager.ResolutionSettings;
+            if (aResIdx > (resSettings.Count - 1) || aResIdx < 0)
+            {
+                aResIdx = resSettings.Count - 1;
+            }
         }
     }
 
     [HarmonyPatch]
     public class Patches
     {
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(GameOptions), nameof(GameOptions.Apply))]
+        public static void Apply_Prefix(Progress progress)
+        {
+            int ResolutionIdx = progress.GetIntValue(GeneralProgress.ProgressInt.Graphics_ResolutionIdx);
+            Plugin.ClampResIdx(ref ResolutionIdx);
+            progress.SetIntValue(GeneralProgress.ProgressInt.Graphics_ResolutionIdx, ResolutionIdx);
+        }
 
+        [HarmonyTranspiler]
+        [HarmonyPatch(typeof(Code.Game.GameRender), "SetGameScreenScale")]
+        public static IEnumerable<CodeInstruction> SetGameScreenScale_Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            /*
+             * Replaces
+             * if (this._gameScreenScaleFactor < 1f)
+             * With
+             * if (this._gameScreenScaleFactor > 1f || this._gameScreenScaleFactor < 1f)
+             * In Code.Game.GameRender.SetGameScreenScale
+            */
+
+            Plugin.log.LogInfo("Patching Code.Game.GameRender.SetGameScreenScale...");
+            CodeMatcher codeMatcher = new CodeMatcher(instructions)
+                        .MatchForward(false,
+                            new CodeMatch(OpCodes.Ldarg_0),
+                            new CodeMatch(i => i.opcode == OpCodes.Ldfld && ((FieldInfo)i.operand).Name == "_gameScreenScaleFactor"),
+                            new CodeMatch(OpCodes.Ldc_R4),
+                            new CodeMatch(OpCodes.Bge_Un)
+                        );
+
+            if (codeMatcher.IsInvalid)
+            {
+                Plugin.log.LogError("Patch failed, fix version is not compatible with game version.");
+                return instructions;
+            }
+
+            CodeInstruction[] checkInstructions = new CodeInstruction[4];
+            for (int i = 0; i < checkInstructions.Length; i++)
+            {
+                checkInstructions[i] = codeMatcher.InstructionAt(i);
+            }
+
+            foreach (var instruction in checkInstructions)
+            {
+                codeMatcher = codeMatcher.InsertAndAdvance(instruction);
+            }
+
+            return codeMatcher
+                   .Advance(-1)
+                   .SetOpcodeAndAdvance(OpCodes.Ble_Un)
+                   .InstructionEnumeration();
+        }
     }
 }
